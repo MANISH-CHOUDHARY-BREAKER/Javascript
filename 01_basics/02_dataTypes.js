cdf6b6c1c3308df5282a3343288a1c0052831274
Manish Choudhary <mc1124400@gmail.com>
2026-09-24 02:25:29 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert( 3 + 3) we are using nodejs, not browser
+
+// code readability should be high
+
+console.log("manish")
+
+let name = "manish"
+let age = 65
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+// null=> standalone value
+// undefined =>
+// symbol => unique 
+
+// object 
+
+console.log(typeof "manish")
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object
